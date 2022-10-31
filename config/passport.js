43db5c189d297a56b341cafd639421b1bc57cc9c
reborn77744 <reborn77744@gmail.com>
2022-10-31 21:02:46 +0800

    init

---
+++ config/passport.js
@@ -0,0 +1,74 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/user-model");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user now");
+  done(null, user._id); //不論使用者是用Google登入或是註冊一個本地帳號，最後都會存進MongoDB中，而MongoDB會自動為每筆資料加入ID，也就是_id
+});
+
+passport.deserializeUser((_id, done) => {
+  console.log("Deserializing user now");
+  User.findById({ _id }).then((user) => {
+    console.log("Found user.");
+    done(null, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    console.log(username, password);
+    User.findOne({ email: username })
+      .then(async (user) => {
+        if (!user) {
+          return done(null, false);
+        }
+        await bcrypt.compare(password, user.password, function (err, result) {
+          if (err) {
+            return done(null, false);
+          }
+          if (!result) {
+            return done(null, false);
+          } else {
+            return done(null, user);
+          }
+        });
+      })
+      .catch((err) => {
+        return done(null, false);
+      });
+  })
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/redirect",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      User.findOne({ googleID: profile.id }).then((foundUser) => {
+        if (foundUser) { //加一個if確保該使用者存在，因為foundUser可能是個null
+          console.log("User already exist");
+          done(null, foundUser);
+        } else {
+          new User({
+            name: profile.displayName,
+            googleID: profile.id,
+            thumbnail: profile.photos[0].value,
+            email: profile.emails[0].value,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("New user created.");
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
