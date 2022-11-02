82fe3c8162ed11a9c110104da5b6ecb219a388e8
reborn77744 <reborn77744@gmail.com>
2022-11-02 16:29:11 +0800

    try

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.get("/", (req, res) => {
   res.render("index", { user: req.user });
 });
 
-app.listen($PORT, hostName, () => {
+app.listen($PORT, () => {
   console.log(`Server running on port 5000.`);
 });
